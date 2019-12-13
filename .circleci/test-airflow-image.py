2b5e17a707a6ca4a050e26341d252d37f607d59c
Steven Miller <sjmiller609@gmail.com>
2019-12-13 00:41:48 +0000

    Reorganize test script

--- .circleci/test-airflow-image.py
+++ .circleci/test-airflow-image.py
@@ -20,6 +20,67 @@ import docker
 from time import sleep, time
 from packaging.version import parse as semantic_version
 
+def test_airflow_in_path(webserver):
+    """ Ensure Airflow is in PATH
+    """
+    assert webserver.exists('airflow'), \
+        "Expected 'airflow' to be in PATH"
+
+def test_maintainer(webserver, docker_client):
+    """ Ensure the Docker image label 'maintainer' is set correctly
+    """
+    maintainer  = get_label(docker_client, 'maintainer')
+    assert maintainer  == "Astronomer <humans@astronomer.io>", \
+        "'maintainer' label should be 'Astronomer <humans@astronomer.io>'"
+
+def test_version(webserver, docker_client):
+    """ Ensure the version of Airflow matches the Docker image label
+    """
+    airflow_version = get_label(docker_client, 'io.astronomer.docker.airflow.version')
+    version_output = webserver.check_output('airflow version')
+    assert airflow_version in version_output
+
+def test_elasticsearch_version(webserver):
+    """ Astronomer runs a version of ElasticSearch that requires
+    our users to run the client code of version 5.5.3 or greater
+    """
+    try:
+        elasticsearch_module = webserver.pip_package.get_packages()['elasticsearch']
+    except KeyError:
+        raise Exception("elasticsearch pip module is not installed")
+    version = elasticsearch_module['version']
+    assert semantic_version(version) >= semantic_version('5.5.3'), \
+        "elasticsearch module must be version 5.5.3 or greater"
+
+@pytest.fixture(scope='session')
+def webserver(request):
+    """ This is the host fixture for testinfra. To read more, please see
+    the testinfra documentation:
+    https://testinfra.readthedocs.io/en/latest/examples.html#test-docker-images
+    """
+    docker_id_db = start_postgres()
+    db_connection_string = f"postgres://postgres:notsecretpassword@{get_ip_from_id(docker_id_db)}:5432"
+    docker_id = subprocess.check_output(
+        ['docker', 'run',
+         '--name', 'webserver',
+         '-e', f"AIRFLOW__CORE__SQL_ALCHEMY_CONN={db_connection_string}",
+         '-d', get_image_name(), 'airflow', 'webserver']).decode().strip()
+
+    yield testinfra.get_host("docker://" + docker_id)
+
+    subprocess.check_call(['docker', 'rm', '-f', docker_id, docker_id_db])
+
+@pytest.fixture(scope='session')
+def docker_client(request):
+    """ This is a text fixture for the docker client,
+    should it be needed in a test
+    """
+    client = docker.from_env()
+    yield client
+    client.close()
+
+## Utility functions below
+
 def get_image_name():
     """ Fetch image name from an environment variable
     and inform the user if they are not using it right
@@ -62,62 +123,3 @@ def get_ip_from_id(_id):
          '-f', '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}',
          _id]
     ).decode().strip()
-
-@pytest.fixture(scope='session')
-def webserver(request):
-    """ This is the host fixture for testinfra. To read more, please see
-    the testinfra documentation:
-    https://testinfra.readthedocs.io/en/latest/examples.html#test-docker-images
-    """
-    docker_id_db = start_postgres()
-    db_connection_string = f"postgres://postgres:notsecretpassword@{get_ip_from_id(docker_id_db)}:5432"
-    docker_id = subprocess.check_output(
-        ['docker', 'run',
-         '--name', 'webserver',
-         '-e', f"AIRFLOW__CORE__SQL_ALCHEMY_CONN={db_connection_string}",
-         '-d', get_image_name(), 'airflow', 'webserver']).decode().strip()
-
-    yield testinfra.get_host("docker://" + docker_id)
-
-    subprocess.check_call(['docker', 'rm', '-f', docker_id, docker_id_db])
-
-@pytest.fixture(scope='session')
-def docker_client(request):
-    """ This is a text fixture for the docker client,
-    should it be needed in a test
-    """
-    client = docker.from_env()
-    yield client
-    client.close()
-
-def test_airflow_in_path(webserver):
-    """ Ensure Airflow is in PATH
-    """
-    assert webserver.exists('airflow'), \
-        "Expected 'airflow' to be in PATH"
-
-def test_maintainer(webserver, docker_client):
-    """ Ensure the Docker image label 'maintainer' is set correctly
-    """
-    maintainer  = get_label(docker_client, 'maintainer')
-    assert maintainer  == "Astronomer <humans@astronomer.io>", \
-        "'maintainer' label should be 'Astronomer <humans@astronomer.io>'"
-
-def test_version(webserver, docker_client):
-    """ Ensure the version of Airflow matches the Docker image label
-    """
-    airflow_version = get_label(docker_client, 'io.astronomer.docker.airflow.version')
-    version_output = webserver.check_output('airflow version')
-    assert airflow_version in version_output
-
-def test_elasticsearch_version(webserver):
-    """ Astronomer runs a version of ElasticSearch that requires
-    our users to run the client code of version 5.5.3 or greater
-    """
-    try:
-        elasticsearch_module = webserver.pip_package.get_packages()['elasticsearch']
-    except KeyError:
-        raise Exception("elasticsearch pip module is not installed")
-    version = elasticsearch_module['version']
-    assert semantic_version(version) >= semantic_version('5.5.3'), \
-        "elasticsearch module must be version 5.5.3 or greater"
