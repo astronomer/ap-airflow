721d2ae7b231b96d2cfa09afe954d576cec5c51e
Kaxil Naik <kaxilnaik@gmail.com>
2020-05-28 14:19:01 +0100

    Release Astro Certified 1.10.7-11
    Tag: https://github.com/astronomer/airflow/releases/tag/v1.10.7+astro.11
    
    User-facing changes are
    
    ### Bug Fixes
    
    - Make loading plugins from entrypoint fault-tolerant (#628) ([commit](https://github.com/astronomer/airflow/commit/ae9d5b7))
    - Pin Azure storage to <0.37.0 (#8833) ([commit](https://github.com/astronomer/airflow/commit/cd12692))
    - Pin Version of Azure Cosmos to <4 ([commit](https://github.com/astronomer/airflow/commit/ff74293))
    - Stop showing Import Errors for Plugins in Webserver ([commit](https://github.com/astronomer/airflow/commit/69dba65))
    
    ### New Features
    - Add support for AWS Secrets Manager as Secrets Backend (#8186)([commit](https://github.com/astronomer/airflow/commit/0dc4736))

--- .circleci/generate_circleci_config.py
+++ .circleci/generate_circleci_config.py
@@ -13,7 +13,7 @@ from jinja2 import Environment, FileSystemLoader
 IMAGE_MAP = collections.OrderedDict([
     ("1.10.5-7", ["alpine3.10", "buster", "rhel7"]),
     ("1.10.6-3", ["alpine3.10", "buster"]),
-    ("1.10.7-11.dev", ["alpine3.10", "buster"]),
+    ("1.10.7-11", ["alpine3.10", "buster"]),
     ("1.10.10-1", ["alpine3.10", "buster"]),
 ])
 
