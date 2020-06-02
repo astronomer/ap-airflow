6c400ad12e6a2a244b8ba86424096aee43f68952
Kaxil Naik <kaxilnaik@gmail.com>
2020-06-02 21:53:06 +0100

    Update AC 1.10.5 Alpine image to install Python 3.7.7 (#90)

--- .circleci/generate_circleci_config.py
+++ .circleci/generate_circleci_config.py
@@ -11,16 +11,27 @@ import re
 from jinja2 import Environment, FileSystemLoader
 
 IMAGE_MAP = collections.OrderedDict([
-    ("1.10.5-7", ["alpine3.10", "buster", "rhel7"]),
+    ("1.10.5-8.dev", ["alpine3.10", "buster", "rhel7"]),
     ("1.10.6-3", ["alpine3.10", "buster"]),
     ("1.10.7-11", ["alpine3.10", "buster"]),
     ("1.10.10-1", ["alpine3.10", "buster"]),
 ])
 
+# Airflow Versions for which we don't publish Python Wheels
+DEV_WHITELIST = ["1.10.5", "1.10.6"]
+
 
 def dev_releases(all_releases):
     """Find dev releases from a list of releases"""
-    return [release for release in all_releases if "dev" in release]
+    return [
+        release for release in all_releases
+        if "dev" in release and get_airflow_version(release) not in DEV_WHITELIST
+    ]
+
+
+def get_airflow_version(ac_version):
+    """Get Airflow Version from the string containing AC Version"""
+    return ac_version.split('-')[0]
 
 
 circle_directory = os.path.dirname(os.path.realpath(__file__))
@@ -38,10 +49,12 @@ def main():
 
     template_env = Environment(loader=FileSystemLoader(searchpath=circle_directory), autoescape=True)
     template_env.filters['dev_releases'] = dev_releases
+    template_env.filters['get_airflow_version'] = get_airflow_version
     template = template_env.get_template("config.yml.j2")
 
     config = template.render(
-        image_map=IMAGE_MAP
+        image_map=IMAGE_MAP,
+        dev_whitelist=DEV_WHITELIST,
     )
     warning_header = "# Warning: automatically generated file\n" + \
                      "# Please edit config.yml.j2, and use the script generate_circleci_config.py\n"
@@ -56,12 +69,13 @@ def replace_version_info():
     Replace the VERSION in all the Dockerfiles with the corresponding VERSION in IMAGE_MAP
     """
     for ac_version, distros in IMAGE_MAP.items():
-        airflow_version = ac_version.split('-')[0]
+        airflow_version = get_airflow_version(ac_version)
         for distro in distros:
             file_name = os.path.join(project_directory, airflow_version, distro, "Dockerfile")
 
             if "dev" in ac_version:
-                ac_version = ac_version.replace("dev", "*")
+                if airflow_version not in DEV_WHITELIST:
+                    ac_version = ac_version.replace("dev", "*")
 
             with open(file_name) as f:
                 file_contents = f.read()
