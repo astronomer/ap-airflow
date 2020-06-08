6067a5ecb6a1b270764a85cf9bd600de47781704
Kaxil Naik <kaxilnaik@gmail.com>
2020-06-08 19:18:46 +0100

    Add 1.10.10-3.dev (#97)

--- .circleci/generate_circleci_config.py
+++ .circleci/generate_circleci_config.py
@@ -13,7 +13,7 @@ from jinja2 import Environment, FileSystemLoader
 IMAGE_MAP = collections.OrderedDict([
     ("1.10.5-8", ["alpine3.10", "buster", "rhel7"]),
     ("1.10.7-12", ["alpine3.10", "buster"]),
-    ("1.10.10-2", ["alpine3.10", "buster"]),
+    ("1.10.10-3.dev", ["alpine3.10", "buster"]),
 ])
 
 # Airflow Versions for which we don't publish Python Wheels
