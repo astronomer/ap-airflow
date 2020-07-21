f49e41d78f0ba9f179d9891713e680a7c23744cb
Kaxil Naik <kaxilnaik@gmail.com>
2020-07-21 23:06:47 +0100

    fixup! fixup! fixup! fixup! fixup! fixup! Run Kubernetes Worker Pods as astro user

--- .circleci/bin/test-airflow-image.py
+++ .circleci/bin/test-airflow-image.py
@@ -179,17 +179,24 @@ def test_airflow_trigger_dags(scheduler):
 def test_airflow_configs(scheduler, docker_client):
     """Verify certain Airflow configurations"""
     distro = get_label(docker_client, "io.astronomer.docker.distro")
-    get_run_as_user = scheduler.check_output("""
-    python -c "from airflow.configuration import conf; print(conf.get('kubernetes','run_as_user'))"
-    """.strip())
 
     # Confirm that run_as_user is the UID for astro user (and not root) for AC images
     if distro == "debian":
-        assert get_run_as_user == "50000"
-    elif distro in ["alpine", "rhel"]:
-        assert get_run_as_user == "100"
+        assert scheduler.check_output(
+            "cat /usr/local/lib/python3.7/site-packages/airflow/config_templates/default_airflow.cfg | "
+            "grep '^run_as_user' | awk '{print $3}'") == "50000"
+    elif distro == "alpine":
+        assert scheduler.check_output(
+            "cat /usr/lib/python3.7/site-packages/airflow/config_templates/default_airflow.cfg | "
+            "grep '^run_as_user' | awk '{print $3}'") == "100"
+    elif distro == "rhel":
+        assert scheduler.check_output(
+            "cat /usr/local/lib/python3.6/site-packages/airflow/config_templates/default_airflow.cfg | "
+            "grep '^run_as_user' | awk '{print $3}'") == "100"
     else:
-        assert get_run_as_user == ""
+        assert scheduler.check_output(
+            "cat /usr/lib/python3.7/site-packages/airflow/config_templates/default_airflow.cfg | "
+            "grep '^run_as_user' | awk '{print $3}'").strip() == ""
 
 
 def test_labels_for_onbuild_image(docker_client):
