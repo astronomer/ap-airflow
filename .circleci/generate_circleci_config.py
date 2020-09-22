beade6801ae32beb37f4c07a96b015458141f2fa
Kaxil Naik <kaxilnaik@gmail.com>
2020-09-22 17:31:43 +0100

    Release AC 1.10.7-15
    https://github.com/astronomer/airflow/releases/tag/v1.10.7%2Bastro.15

--- .circleci/generate_circleci_config.py
+++ .circleci/generate_circleci_config.py
@@ -12,7 +12,7 @@ from jinja2 import Environment, FileSystemLoader
 
 IMAGE_MAP = collections.OrderedDict([
     ("1.10.5-11.dev", ["alpine3.10", "buster", "rhel7"]),
-    ("1.10.7-15.dev", ["alpine3.10", "buster"]),
+    ("1.10.7-15", ["alpine3.10", "buster"]),
     ("1.10.10-5", ["alpine3.10", "buster"]),
     ("1.10.12-1.dev", ["alpine3.10", "buster"]),
 ])
