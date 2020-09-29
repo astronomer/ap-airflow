8c9589347ac695bbf5603c0c39834a9161c98841
Kaxil Naik <kaxilnaik@gmail.com>
2020-09-29 23:18:27 +0100

    Release AC 1.10.12-1
    Release: https://github.com/astronomer/airflow/releases/tag/v1.10.12%2Bastro.1

--- .circleci/generate_circleci_config.py
+++ .circleci/generate_circleci_config.py
@@ -14,7 +14,7 @@ IMAGE_MAP = collections.OrderedDict([
     ("1.10.5-11.dev", ["alpine3.10", "buster", "rhel7"]),
     ("1.10.7-15", ["alpine3.10", "buster"]),
     ("1.10.10-5", ["alpine3.10", "buster"]),
-    ("1.10.12-1.dev", ["alpine3.10", "buster"]),
+    ("1.10.12-1", ["alpine3.10", "buster"]),
 ])
 
 # Airflow Versions for which we don't publish Python Wheels
