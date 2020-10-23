1580f24146b3bd00ebece8df43dca2dff837eb7a
Kaxil Naik <kaxilnaik@gmail.com>
2020-10-23 13:45:36 +0100

    Add 1.10.12-2.dev

--- .circleci/generate_circleci_config.py
+++ .circleci/generate_circleci_config.py
@@ -14,7 +14,7 @@ IMAGE_MAP = collections.OrderedDict([
     ("1.10.5-11", ["alpine3.10", "buster", "rhel7"]),
     ("1.10.7-15", ["alpine3.10", "buster"]),
     ("1.10.10-5", ["alpine3.10", "buster"]),
-    ("1.10.12-1", ["alpine3.10", "buster"]),
+    ("1.10.12-2.dev", ["alpine3.10", "buster"]),
     ("2.0.0-1.dev", ["buster"]),
 ])
 
