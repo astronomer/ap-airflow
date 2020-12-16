d951572d4ccb9f3658941901d7cd4f51e712391b
Kaxil Naik <kaxilnaik@apache.org>
2020-12-16 15:19:00 +0000

    Add Dev versions for 1.10.7-17, 1.10.10-7 and 1.10.14-2

--- .circleci/generate_circleci_config.py
+++ .circleci/generate_circleci_config.py
@@ -12,10 +12,10 @@ from jinja2 import Environment, FileSystemLoader
 
 IMAGE_MAP = collections.OrderedDict([
     ("1.10.5-11", ["alpine3.10", "buster", "rhel7"]),
-    ("1.10.7-16", ["alpine3.10", "buster"]),
-    ("1.10.10-6", ["alpine3.10", "buster"]),
+    ("1.10.7-17.dev", ["alpine3.10", "buster"]),
+    ("1.10.10-7.dev", ["alpine3.10", "buster"]),
     ("1.10.12-2", ["alpine3.10", "buster"]),
-    ("1.10.14-1", ["buster"]),
+    ("1.10.14-2.dev", ["buster"]),
     ("2.0.0-1.dev", ["buster"]),
 ])
 
