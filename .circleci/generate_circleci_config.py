3bc0e96ecdf0bc862e10cb11c3ccb87ccb318619
Kaxil Naik <kaxilnaik@gmail.com>
2021-03-30 00:17:42 +0100

    Add 2.0.0-4.dev
    This commit adds 2.0.0-4.dev image from
     https://github.com/astronomer/airflow/commits/v2-0-0

--- .circleci/generate_circleci_config.py
+++ .circleci/generate_circleci_config.py
@@ -17,7 +17,7 @@ IMAGE_MAP = collections.OrderedDict([
     ("1.10.12-4", ["alpine3.10", "buster"]),
     ("1.10.14-3", ["buster"]),
     ("1.10.15-1", ["buster"]),
-    ("2.0.0-3", ["buster"]),
+    ("2.0.0-4.dev", ["buster"]),
 ])
 
 # Airflow Versions for which we don't publish Python Wheels
