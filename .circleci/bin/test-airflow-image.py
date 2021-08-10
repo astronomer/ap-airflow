2fc11e042265cc2e176d6c1384b364fb78e46c4a
Collin McNulty <collin.mcnulty@gmail.com>
2021-08-10 14:44:50 -0500

    attempt to test pip version only for changed images

--- .circleci/bin/test-airflow-image.py
+++ .circleci/bin/test-airflow-image.py
@@ -85,6 +85,9 @@ def test_version(webserver, docker_client):
         post_fix_version_astro = ac_version.rsplit('-')[-1]
     assert post_fix_version_astro == ac_version_postfix_output, \
         f"Incorrect post-fix version in {ac_version_output}"
+    if get_label(docker_client, "io.astronomer.docker.distro") == "debian"
+        pip_ver = webserver.check_output("pip --version")
+        assert pip_ver == "21.2.3"
 
 
 def test_elasticsearch_version(webserver):
