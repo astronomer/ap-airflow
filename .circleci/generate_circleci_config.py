cb7c9a92a2430fd959ccb4b8f6077da9b9fd83f2
Jed Cunningham <66968678+jedcunningham@users.noreply.github.com>
2021-08-24 17:40:39 -0600

    Create AC 2.2.0-1 dev image (#297)
    Publishes the 2.2.0 dev image so it can be tested.

--- .circleci/generate_circleci_config.py
+++ .circleci/generate_circleci_config.py
@@ -21,6 +21,7 @@ IMAGE_MAP = collections.OrderedDict([
     ("2.1.0-3", ["buster"]),
     ("2.1.1-2", ["buster"]),
     ("2.1.3-1", ["buster"]),
+    ("2.2.0-1.dev", ["buster"]),
 ])
 
 # Airflow Versions for which we don't publish Python Wheels
