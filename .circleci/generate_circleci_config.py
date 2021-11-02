d9d2026cb61455ff360b6bcd4fd7ceb64cc17070
Jed Cunningham <66968678+jedcunningham@users.noreply.github.com>
2021-11-02 13:44:31 -0600

    Release AC 2.2.0-3

--- .circleci/generate_circleci_config.py
+++ .circleci/generate_circleci_config.py
@@ -21,7 +21,7 @@ IMAGE_MAP = collections.OrderedDict([
     ("2.1.1-4", ["buster"]),
     ("2.1.3-2", ["buster"]),
     ("2.1.4-2", ["buster"]),
-    ("2.2.0-3.dev", ["bullseye", "buster"]),
+    ("2.2.0-3", ["bullseye", "buster"]),
     ("2.2.1-1", ["bullseye", "buster"]),
 ])
 
