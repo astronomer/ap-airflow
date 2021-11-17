41fd3ef7beef31ced8b83f82fc400779386f96da
blag <blag@users.noreply.github.com>
2021-11-17 11:05:46 -0800

    Dynamically generate the rebase-astro-main workflow
    Publish a dev image from main

--- .circleci/common.py
+++ .circleci/common.py
@@ -14,12 +14,22 @@ def dev_releases(all_releases):
     """Find dev releases from a list of releases"""
     return [
         release for release in all_releases
-        if "dev" in release and get_airflow_version(release) not in DEV_ALLOWLIST
+        if is_dev_release(release) and get_airflow_version(release) not in DEV_ALLOWLIST
     ]
+
+
+def is_dev_release(version):
+    return "dev" in version or is_edge_build(version)
+
+
+def is_edge_build(version):
+    return "main" in version
 
 
 def get_airflow_version(ac_version):
     """Get Airflow Version from the string containing AC Version"""
+    if is_edge_build(ac_version):
+        return "main"
     return ac_version.split('-')[0]
 
 
@@ -37,6 +47,7 @@ IMAGE_MAP = collections.OrderedDict([
     ("2.2.0-4", ["bullseye", "buster"]),
     ("2.2.1-2", ["bullseye", "buster"]),
     ("2.2.2-1", ["bullseye"]),
+    ("main.dev", ["bullseye"]),
 ])
 
 # Airflow Versions for which we don't publish Python Wheels
