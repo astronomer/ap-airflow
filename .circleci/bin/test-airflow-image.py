49d6415e56854fe4bc7370ab1ec868c1f58c3e67
blag <blag@users.noreply.github.com>
2022-03-28 15:20:55 -0700

    Handle dev versions

--- .circleci/bin/test-airflow-image.py
+++ .circleci/bin/test-airflow-image.py
@@ -78,9 +78,9 @@ def test_version(webserver, docker_client):
     assert "+astro." in ac_version_output
     ac_version_postfix_output = ac_version_output.rsplit('+astro.')[-1]
 
-    # Example: 2.0.2.post2-dev2
+    # Example: 2.0.2-dev2 2.0.2.post2.dev2
     if "dev" in ac_version:
-        ac_version = ac_version.rsplit('-dev')[0]
+        ac_version = ac_version.rsplit('-dev')[0].rsplit('.dev')[0]
         post_fix_version_astro = ac_version.rsplit('.post')[-1]
     elif ".post" in ac_version:
         # Example: 2.2.4.post3
