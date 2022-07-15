bc2e21875aeab73a67f1828a392cb53f01d25d2c
Jed Cunningham <66968678+jedcunningham@users.noreply.github.com>
2022-07-15 15:47:31 -0600

    Fix openssl cve in 1.10.15

--- .circleci/common.py
+++ .circleci/common.py
@@ -34,7 +34,7 @@ def get_airflow_version(ac_version):
 
 
 IMAGE_MAP = collections.OrderedDict([
-    ("1.10.15-8", ["buster"]),
+    ("1.10.15-9-dev", ["buster"]),
     ("2.1.4-9-dev", ["buster"]),
     ("2.3.3-1", ["bullseye"]),
     ("main-dev", ["bullseye"]),
