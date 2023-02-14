5d529992d4fcfc729a83bf8e9b72c8f61b4974f0
Jed Cunningham <66968678+jedcunningham@users.noreply.github.com>
2023-02-14 10:34:01 -0700

    Release 2.4.3-5 and 2.3.4-10

--- .circleci/common.py
+++ .circleci/common.py
@@ -34,6 +34,6 @@ def get_airflow_version(ac_version):
 
 
 IMAGE_MAP = collections.OrderedDict([
-    ("2.3.4-10-dev", ["bullseye"]),
-    ("2.4.3-5-dev", ["bullseye"]),
+    ("2.3.4-10", ["bullseye"]),
+    ("2.4.3-5", ["bullseye"]),
 ])
